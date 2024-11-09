9c0e22a7f31a4301fe9f31ef0d1ea32fd1d1e465
Karine <karinedeveloper@gmail.com>
2024-11-09 20:02:02 -0300

    feat: add authtentication with clerk

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import { Button } from "./_components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
+import { dark } from "@clerk/themes";
+import { redirect } from "next/navigation";
 
-const Home = () => {
+const Home = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/login");
+  }
   return (
-    <div className="flex justify-center">
-      <Button>Hello</Button>
+    <div className="flex h-full items-center justify-center">
+      <UserButton showName></UserButton>
     </div>
   );
 };
